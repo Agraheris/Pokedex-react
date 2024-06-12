7771921ba709e33f2205cf94ec999c8525b6e375
clement Vigouroux <clement.vigouroux@gmail.com>
2024-06-12 16:18:18 +0200

    add state.1

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,47 @@ import PokemonCard from './components/PokemonCard';
 
 const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <PokemonCard pokemon={pokemonList[0]} />
-  )
-    
-}
+  const [pokemonIndex, setPokemonIndex] = useState(0)
+  const previousPokemon = () => {
+    setPokemonIndex((pokemonIndex) => (pokemonIndex -1));
+  };
+    const nextPokemon = () => {
+      setPokemonIndex((pokemonIndex) => (pokemonIndex +1 ));
+    };
+    return (
+      <div>
+        <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+        <div>
+          {pokemonIndex >0 ? <button onClick={previousPokemon}>Précédent</button> : <p></p>}
+          {pokemonIndex <pokemonList.length-1 ? <button onClick={nextPokemon}>Suivant</button> : <p></p>}
+        </div>
+      </div>
+    );
+  }
 
 export default App
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,16 @@
 import PropTypes from "prop-types";
-function PokemonCard ({pokemon}){
-    return (<figure>
-      {pokemon.imgSrc ?  
-        <img src= {pokemon.imgSrc} alt={pokemon.name}/> : <p>????</p>}
-        <figcaption>{pokemon.name}</figcaption>
-        </figure>
-    )
 
+function PokemonCard ({pokemon}){
+    return (
+      <figure>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>????</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  );
 }
 
 PokemonCard.propTypes = {
